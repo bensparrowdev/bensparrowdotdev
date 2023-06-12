06394d19e6c5ba9beedf73d4da949f4addad6e66
Ben Sparrow <>
2023-06-12 22:25:14 +0100

    404 page and sitemap config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,15 @@
 import { defineConfig } from 'astro/config';
 import image from "@astrojs/image";
 import react from "@astrojs/react";
-
 import tailwind from "@astrojs/tailwind";
 
+
 // https://astro.build/config
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [image(), react(), tailwind()],
+  integrations: [image(), react(), tailwind(), sitemap()],
   server: {
     host: true
   }
