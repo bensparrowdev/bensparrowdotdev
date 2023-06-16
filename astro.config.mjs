6301d8b15d9ef9a6c51e73a7559acc2da7e7a92b
Ben Sparrow <>
2023-06-16 09:19:07 +0100

    tidy / trigger deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,6 @@ import { defineConfig } from 'astro/config';
 import image from "@astrojs/image";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-
-
-// https://astro.build/config
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
