81134a28b701390d405d4c0b40fe6f00fb98170f
Ben Sparrow <>
2024-04-19 12:33:48 +0100

    mobile optimised + colours

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,42 +1,42 @@
 /** @type {import('tailwindcss').Config} */
-const colors = require("tailwindcss/colors");
+const colors = require('tailwindcss/colors');
 
-delete colors["lightBlue"];
-delete colors["warmGray"];
-delete colors["trueGray"];
-delete colors["coolGray"];
-delete colors["blueGray"];
+delete colors['lightBlue'];
+delete colors['warmGray'];
+delete colors['trueGray'];
+delete colors['coolGray'];
+delete colors['blueGray'];
 
 module.exports = {
-  darkMode: "class",
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  darkMode: 'class',
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     container: {
       padding: {
-        DEFAULT: "1rem",
-        sm: "0rem",
+        DEFAULT: '1rem',
+        sm: '0rem',
       },
       center: true,
     },
     colors: {
-      primary: "#021013",
-      secondary: "#FFFDFC",
-      accent: "#DCEEEE",
-      accent2: "#7AFFF2",
-      body: "#545454",
-      primary_dark: "#E1E1E1",
-      secondary_dark: "#041F25",
-      accent_dark: "#213939",
-      accent2_dark: "#008F81",
-      body_dark: "#C6C6C6",
-      bg_text_dark: "#062B3A",
-      invisible: "#FFFFFF00",
+      primary: '#021013',
+      secondary: '#FFFDFC',
+      accent: '#DCEEEE',
+      accent2: '#7692FF',
+      body: '#545454',
+      primary_dark: '#E1E1E1',
+      secondary_dark: '#041F25',
+      accent_dark: '#213939',
+      accent2_dark: '#008F81',
+      body_dark: '#C6C6C6',
+      bg_text_dark: '#062B3A',
+      invisible: '#FFFFFF00',
     },
     extend: {
       fontFamily: {
-        eb_Garamond: ["EB Garamond", "serif"],
-        poppins: ["Poppins", "sans- serif"],
-        ubuntu_mono: ["Ubuntu Mono", "monospace"],
+        eb_Garamond: ['EB Garamond', 'serif'],
+        poppins: ['Poppins', 'sans- serif'],
+        ubuntu_mono: ['Ubuntu Mono', 'monospace'],
       },
       colors,
     },
